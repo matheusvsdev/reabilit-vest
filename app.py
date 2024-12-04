459f6636272fb1fbadbbccb775bab5581e0b4097
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-12-04 00:12:01 -0300

    Implementation of Constants, Enum Level and Enum SimulationType

--- app.py
+++ app.py
@@ -2,3 +2,21 @@ import pygame
 import random
 import sys
 
+# Constantes
+BALL_RADIUS = 20
+BALL_SPEED = 50.0
+SIMULATION_TIME = 60 # 1 minuto
+
+# Enum para os níveis de dificuldade
+class Level:
+    FACIL = 1
+    MEDIO = 2
+    DIFICIL = 3
+    VOLTAR = 4
+
+# Enum para os tipos de simulação
+class SimulationType:
+    VERTICAL =1
+    HORIZONTAL = 2
+    FIND_THE_BALL = 3
+    RANDOM = 4
